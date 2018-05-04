26a2ae5682190d48266602074077918cdf5e7578
Bastien <bastien.nicoud@gmail.com>
2018-05-04 13:50:45 +0200

    Add append and prepend methods to the list class

--- algorithmic/list/ex0/list.rb
+++ algorithmic/list/ex0/list.rb
@@ -1,7 +1,7 @@
 class List
 
-  @firstNode = Node;
-  @lastNode = Node;
+  @firstNode = nil;
+  @lastNode = nil;
 
   def initialize()
   end
@@ -10,20 +10,53 @@ class List
     # Check if the list is empty
     if (@firstNode.empty? && @lastNode.empty?) {
       # Create the element il all node
-      @firstNode = Node.new(nodeValue)
-      @firstNode.next = @firstNode
-      @firstNode.prev = @firstNode
+      @firstNode      = Node.new(nodeValue)
+      @firstNode.next = nil
+      @firstNode.prev = nil
+      @lastNode       = nil
+    # If one element in the list
+    } elsif (@lastNode == nil) {
+      @lastNode       = Node.new(nodeValue)
+      @lastNode.next  = nil
+      @lastNode.prev  = @firstNode
+      @firstNode.next = @lastNode
+    # Create a new element and add it to the end
+    } else {
+      tmp       = Node.new(nodeValue)
+      tmp.next  = nil
+      tmp.prev  = @lastNode
+      @lastNode = tmp
     }
-    Node.new(nodeValue)
   end
 
   def prepend(nodeValue)
+    if (@firstNode.empty? && @lastNode.empty?) {
+      # Create the element il all node
+      @firstNode      = Node.new(nodeValue)
+      @firstNode.next = nil
+      @firstNode.prev = nil
+      @lastNode       = nil
+    # If one element in the list
+    } elsif (@lastNode == nil) {
+      @lastNode       = @firstNode
+      @firstNode      = Node.new(nodeValue)
+      @firstNode.prev = nil
+      @firstNode.next = @lastNode
+      @lastNode.prev  = @firstNode
+    } else {
+      tmp      = Node.new(nodeValue)
+      tmp.next = @firstNode
+      tmp.prev = nil
+      @firstNode = tmp
+    }
   end
 
   def getFirst
+    @firstNode
   end
 
   def getLast
+    @lastNode
   end
 
   def takeFirst
@@ -36,6 +69,7 @@ class List
   end
 
   def empty?
+    if (@firstNode.empty? && @lastNode.empty?)
   end
 
   def each
