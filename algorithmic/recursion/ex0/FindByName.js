14d69402bb8084097766bb03d6708c68c47eb90f
Bastien <bastien.nicoud@gmail.com>
2018-05-04 08:17:56 +0200

    Change js version of FindByName

--- algorithmic/recursion/ex0/FindByName.js
+++ algorithmic/recursion/ex0/FindByName.js
@@ -1,12 +1,29 @@
-import fs from 'fs'
-import path from 'path'
+const fs = require('fs')
+const path = require('path')
 
 // Search a file
-let search = (path, needle) => {
-  let results = []
-  fs.readdirSync(path).forEach((file) => {
-    
+let search = function (rootpath, needle, accumulator) {
+  // Iterates the folder
+  fs.readdirSync(rootpath).forEach(function (file) {
+    // Concat path and filename
+    let filepath = path.join(rootpath, file)
+    // Chek is is a file
+    if (fs.statSync(filepath).isFile()) {
+      // Match de needle
+      if (file.match(needle)) {
+        accumulator.push(filepath)
+      }
+    // If is a directory, recurse
+    } else if (fs.statSync(filepath).isDirectory()) {
+      search(filepath, needle, accumulator)
+    }
   })
+  return accumulator
 }
 
-console.log(search('/users/bastien/Sites/POO2', /README.md/))
+// Create an accumulator
+let accumulator = []
+// Perform the search
+search('/users/bastien/Sites/POO2', /README.md/igm, accumulator)
+// Display the search
+console.log(accumulator)
