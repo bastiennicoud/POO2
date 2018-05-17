e1263b1cf0c81d973981d7a01d2831beaefd0f8b
Bastien <bastien.nicoud@gmail.com>
2018-05-17 10:51:44 +0200

    Add tests for queues

--- algorithmic/queue/withList/main.rb
+++ algorithmic/queue/withList/main.rb
@@ -1,2 +1,22 @@
 require_relative('queue')
 
+puts Date.now()
+
+queue = Queue.new()
+a = 0
+
+while a < 100 do
+  queue.enqueue(a)
+  a += 1
+end
+
+puts Date.now()
+
+b = 0
+
+while b < 100 do
+  puts queue.dequeue
+  a += 1
+end
+
+puts Date.now()
