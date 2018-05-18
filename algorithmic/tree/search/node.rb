1adb50ca9b12d9d488b38d845d40dc18ba49de7a
Bastien <bastien.nicoud@gmail.com>
2018-05-18 12:07:51 +0200

    Add method to append new nodes on childrens

--- algorithmic/tree/search/node.rb
+++ algorithmic/tree/search/node.rb
@@ -1,10 +1,15 @@
 class Node
-  
-  def initialize(parent = nil, val)
+
+  def initialize(val, parent = nil, childrens = {})
     @parent = parent
+    @childrens = childrens
     @value = val
   end
-  
+
   attr_accessor(:parent, :value)
-  
+
+  def add_children(char)
+    @childrens[char] = Node.new(char, self)
+  end
+
 end
--- algorithmic/tree/search/word_search_tree.rb
+++ algorithmic/tree/search/word_search_tree.rb
@@ -1,25 +1,28 @@
 require_relative('node')
 
 class WordSearchTree
-  
+
   def initialize(elements = [])
+    @root = Node.new(nil)
     elements.each do |el|
       add_word(el)
     end
   end
-  
+
   def add_word(word)
     word.split("").each do |char|
-      puts char
+      # Check if the char exists une the childs nodes
+      # Create the char in the root childrends
+      # pass to the next char
     end
   end
-  
+
   def search()
-    
+
     # When the word is found
     recompose_word(leafNode, accumulator = '')
   end
-  
+
   def recompose_word(leafNode, accumulator)
     accumulator = `#{leafNode.value}#{accumulator}`
     if leafNode.parent.nil?
@@ -28,5 +31,5 @@ class WordSearchTree
       recompose_word(leafNode.parent, accumulator)
     end
   end
-  
+
 end
